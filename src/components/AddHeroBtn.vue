8c92fa0b47f6f507227bfb849a28a4b8c01d50ac
Tushar Garg <46694524+digibuddy45@users.noreply.github.com>
2020-12-19 23:03:30 +0530

    initial commit

---
+++ src/components/AddHeroBtn.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "add-hero-btn",
+  props: {
+    buttonName: String,
+  },
+};
+</script>
+
+<template>
+  <a
+    class="f6 link dim br3 ph3 pv2 mb2 dib white bg-black"
+    @click="$emit('add-hero', true)"
+    >{{ buttonName }}</a
+  >
+</template>
---
+++ src/components/TwitterShortProfile.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "twitter-short-profile",
+  props: {
+    superhero: Object,
+  },
+  methods: {
+    isFollowing(hero) {
+      this.$set(hero, "isFollowing", !hero.isFollowing);
+    },
+  },
+};
+</script>
+
+<template>
+  <article class="dt w-100 bb b--black-05 pb2 mt2" href="#0">
+    <div class="dtc w2 w3-ns v-mid">
+      <img
+        :src="superhero.pic"
+        class="ba b--black-10 db br-100 w2 w3-ns h2 h3-ns"
+      />
+    </div>
+    <div class="dtc v-mid pl3">
+      <h1 class="f6 f5-ns fw6 lh-title black mv0">{{ superhero.name }}</h1>
+      <h2 class="f6 fw4 mt0 mb0 black-60">{{ superhero.twitter }}</h2>
+    </div>
+    <div class="dtc v-mid">
+      <form class="w-100 tr" @submit.prevent="isFollowing(superhero)">
+        <button
+          class="f6 link dim br2 ph3 pv2 mb2 dib white bg-black"
+          type="submit"
+        >
+          {{ superhero.isFollowing == true ? "Unfollow" : "Follow" }}
+        </button>
+      </form>
+    </div>
+  </article>
+</template>
